868f36f804653201dd0c2138e60f5e2cdcab9286
AndresTabor <andrestaborda43@gmail.com>
2022-09-11 19:55:28 -0500

    corrigiendo errores de importacion

--- Back/applications/app-proxy/src/main/java/org/example/cardgame/websocket/ApplicationConfig.java
+++ Back/applications/app-proxy/src/main/java/org/example/cardgame/websocket/ApplicationConfig.java
@@ -1,16 +1,13 @@
 package org.example.cardgame.websocket;
 
 
+
+
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
